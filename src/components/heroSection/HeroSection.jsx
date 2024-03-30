08896e983e18594e036c7b3e930dec2a4525e7ed
unipace-main <162615596+unipace-main@users.noreply.github.com>
2024-03-30 11:09:06 +0530

    Update HeroSection.jsx-path changed in line 12

--- src/components/heroSection/HeroSection.jsx
+++ src/components/heroSection/HeroSection.jsx
@@ -2,13 +2,14 @@ import { Typography } from '@material-tailwind/react'
 import React, { useContext } from 'react'
 import myContext from '../../context/data/myContext';
 
+
 function HeroSection() {
     const context = useContext(myContext);
     const { mode } = context;
 
     // Define the background image URL
     const backgroundImageUrl = mode === 'light' 
-        ? 'url("src\assets\linkedin 4.png")' 
+        ? 'url("../../../assets/linkedin 4.png")';
         : 'url("https://drive.google.com/file/d/1sOrQDHZTW7YDuvdG2UDO4ONpKQ3b_km_/view?usp=sharing")'; // Change 'your-dark-image-url.jpg' to your actual dark image URL
 
     return (
